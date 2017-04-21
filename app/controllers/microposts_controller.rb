53702972493a1a4c00746d03d9ebdafe38df9165
akihiro mochizuki <a.mochizuki13@gmail.com>
2017-04-21 18:03:27 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,8 +8,9 @@ class MicropostsController < ApplicationController
       flash[:success] = "メッセージを投稿しました。"
       redirect_to root_url
     else
+      @microposts = current_user.feed_microposts.order("created_at DESC").page(params[:page])
       flash.now[:danger] = "メッセージの投稿に失敗しました。"
-      render "toppages/new"
+      render "toppages/index"
     end
   end
 
